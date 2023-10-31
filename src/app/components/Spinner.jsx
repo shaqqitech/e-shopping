cf4f5927d04b8ae4ce5374400efdc9f19f9d6ba8
Mehar Ali <mehar0alii@gmail.com>
2023-10-31 11:15:21 +0500

    spinner positon changes

--- src/app/components/Spinner.jsx
+++ src/app/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="w-screen h-screen grid place-content-center">
+    <div className="w-screen h-screen grid place-content-center relative -top-10 left-0">
       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-black dark:border-violet-400"></div>
     </div>
   );
